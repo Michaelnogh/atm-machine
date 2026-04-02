a6b4376a92a866d145108e6502add56bf632d535
tgbbb43 <tgbbb43@gmail.com>
2026-04-03 00:01:44 +0300

    End for today 2.4.26

--- atm_project/models.py
+++ atm_project/models.py
@@ -1,3 +1,5 @@
+from eror import eror_list
+from datetime import datetime
 history1 =[]
 history2 =[]
 history3 =[]
@@ -14,5 +16,16 @@ class Account:
 account1 = Account("michael",123456,1234,5000,True,history1,False)
 account2 = Account("eli",838355,5678,15000,True,history2,False)
 account3 = Account("GOD",111111,1818,100000,True,history3,True)
+all_accounts = [account1, account2, account3]
 
+def enter_pin():
+    try:
+     pin_input = int(input("enter pin: "))
+    except:
+        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+        error_msg = f"[{now}] ERROR: User entered Letters, not numbers."
+        eror_list.append(error_msg)
         
+
+         
+    
