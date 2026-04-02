fea3e5385b66d0a5e0d436e17137897838d3834f
tgbbb43 <tgbbb43@gmail.com>
2026-04-02 22:56:34 +0300

    [Account list]

--- atm_project/models.py
+++ atm_project/models.py
@@ -1,15 +1,18 @@
-history = ["hu"]
-
+history1 =[]
+history2 =[]
+history3 =[]
 class Account:
-    def __init__(self,name,account_id,pin,balance,active_account,transaction_history):
+    def __init__(self,name,account_id,pin,balance,active,transaction_history,admin_convictions):
         self.name = name
         self.account_id = account_id
         self.pin = pin
         self.balance = balance
-        self.active_account = active_account
+        self.active = active
         self.transaction_history = transaction_history
+        self.admin_convictions = admin_convictions
+        
+account1 = Account("michael",123456,1234,5000,True,history1,False)
+account2 = Account("eli",838355,5678,15000,True,history2,False)
+account3 = Account("GOD",111111,1818,100000,True,history3,True)
 
-account1 = Account("michel",123456,1234,50000,True,history1)
-account2 = Account()
-
-
+        
