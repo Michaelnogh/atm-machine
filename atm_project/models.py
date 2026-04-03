3b48e5bc2bb5e76f38ab7a7d87857dd929d88a5c
tgbbb43 <tgbbb43@gmail.com>
2026-04-03 15:24:55 +0300

    [bug]

--- atm_project/models.py
+++ atm_project/models.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from storage import *
+import json
 
 class Account:
     def __init__(self,name,account_id,pin,balance,active,transaction_history,admin_convictions):
@@ -11,25 +12,49 @@ class Account:
         self.transaction_history = transaction_history
         self.admin_convictions = admin_convictions
         
-account1 = Account("michael",123456,1234,5000,True,history1,False)
-account2 = Account("eli",838355,5678,15000,True,history2,False)
-account3 = Account("GOD",111111,1818,100000,True,history3,True)
-all_accounts = [account1, account2, account3]
 
-def enter_pin():
+
+def load_data():
     try:
-     pin_input = int(input("enter pin: "))
-     for ac in all_accounts:
-        if pin_input == ac.pin:
-             return True        
-        else:
-             return False
-    except:
-        now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-        error_msg = f"[{now}] ERROR: User entered Letters, not numbers in pin."
-        eror_list.append(error_msg)
-        
+        with open("accounts.json", "r") as f:
+            # טעינת הרשימה מהקובץ
+            data = json.load(f)
+            
+            # הפיכה של כל מילון ב-JSON לאובייקט Account
+            all_accounts = []
+            for item in data:
+                acc = Account(
+                    item["name"], 
+                    item["account_id"], 
+                    item["pin"], 
+                    item["balance"], 
+                    item["active"], 
+                    item["transaction_history"], 
+                    item["admin_convictions"]
+                )
+                all_accounts.append(acc)
+            return all_accounts
+    except FileNotFoundError:
+        print("קובץ הנתונים לא נמצא!")
+        return []
 
+def save_data(all_accounts):
+    # הפיכת כל האובייקטים חזרה למילונים פשוטים
+    data_to_save = []
+    for acc in all_accounts:
+        data_to_save.append({
+            "name": acc.name,
+            "account_id": acc.account_id,
+            "pin": acc.pin,
+            "balance": acc.balance,
+            "active": acc.active,
+            "transaction_history": acc.transaction_history,
+            "admin_convictions": acc.admin_convictions
+        })
+    
+    # כתיבה לקובץ
+    with open("accounts.json", "w") as f:
+        json.dump(data_to_save, f, indent=4)
 
          
     
--- atm_project/storage.py
+++ atm_project/storage.py
@@ -1,4 +1,20 @@
-eror_list =[]
-history1 =[]
-history2 =[]
-history3 =[]
+import json
+
+from datetime import *
+import os
+
+MAIN_DIR = os.path.dirname(os.path.abspath(__file__))
+DATA_FILE = os.path.join(MAIN_DIR, "data.json")
+
+def all_clients(filename=DATA_FILE):
+    
+    #read the data.json and store client data in data var 
+    with open(filename, "r") as file:
+        data = json.load(file)
+        return data
+
+def main():
+    print(all_clients())
+
+if __name__ == "__main__":
+    main()    
